bad46c7ca47bfbc15f33e5fdf44f2d1dbab0dec6
Ricardo Fernandes <ricardo.fernandes@devoteam.com>
2021-02-05 13:34:05 +0000

    changes

--- data/scripts/actions/object/imbuement_shrine.lua
+++ data/scripts/actions/object/imbuement_shrine.lua
@@ -1,7 +1,7 @@
 local imbuement = Action()
 
 function imbuement.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:getStorageValue(Storage.ForgottenKnowledge.Tomes) ~= 1 then
+	if player:getStorageValue(Storage.ForgottenKnowledge.Tomes) < 0 then
 		return player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You did not collect enough knowledge from the ancient Shapers. Visit the Shaper temple in Thais for help.")
 	end
 
--- data/stages.lua
+++ data/stages.lua
@@ -4,72 +4,54 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 8,
-		multiplier = 7
-	}, {
-		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6
-	}, {
-		minlevel = 21,
 		maxlevel = 50,
-		multiplier = 5
+		multiplier = 100
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 4
+		multiplier = 80
 	}, {
 		minlevel = 101,
-		multiplier = 2
+		maxlevel = 150,
+		multiplier = 60
+	}, {
+		minlevel = 151,
+		maxlevel = 200,
+		multiplier = 40
+	}, {
+		minlevel = 201,
+		maxlevel = 250,
+		multiplier = 30
+	}, {
+		minlevel = 251,
+		maxlevel = 300,
+		multiplier = 20
+	}, 
+	{
+		minlevel = 301,
+		maxlevel = 400,
+		multiplier = 15
+	}, {
+		minlevel = 401,
+		maxlevel = 500,
+		multiplier = 10
+	}, 
+	{
+		minlevel = 501,
+		multiplier = 5
 	}
 }
 
 skillsStages = {
 	{
-		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15
-	}, {
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 10
-	}, {
-		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6
-	}, {
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 4
-	}, {
-		minlevel = 126,
-		multiplier = 2
+		minlevel = 1,
+		multiplier = 25
 	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 0,
-		maxlevel = 60,
-		multiplier = 10
-	}, {
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7
-	}, {
-		minlevel = 81,
-		maxlevel = 100,
 		multiplier = 5
-	}, {
-		minlevel = 101,
-		maxlevel = 110,
-		multiplier = 4
-	}, {
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3
-	}, {
-		minlevel = 126,
-		multiplier = 2
 	}
 }
