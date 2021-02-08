713f6906dbc42f83661259ec1e074f8fbcac734a
Ricardo Fernandes <ricardo.fernandes@devoteam.com>
2021-02-08 18:57:53 +0000

    adds changes to trade npcs and updates knight exori spells

--- data/scripts/actions/object/imbuement_shrine.lua
+++ data/scripts/actions/object/imbuement_shrine.lua
@@ -1,9 +1,9 @@
 local imbuement = Action()
 
 function imbuement.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:getStorageValue(Storage.ForgottenKnowledge.Tomes) < 0 then
-		return player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You did not collect enough knowledge from the ancient Shapers. Visit the Shaper temple in Thais for help.")
-	end
+	 if player:getStorageValue(Storage.ForgottenKnowledge.Tomes) < 0 then
+		 return player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You did not collect enough knowledge from the ancient Shapers. Visit the Shaper temple in Thais for help.")
+	 end
 
 	player:sendImbuementPanel(target, true)
 	return true
